c420da9a170de912c6a85d8a102ec4f748ca7bf9
YousefAlbasel <albaselofficial@gmail.com>
2023-02-18 15:24:02 -0800

    Committed problem 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+i}
