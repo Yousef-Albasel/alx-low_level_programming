e3595f4b05ac2f986c9965b919fa08ad233ca475
YousefAlbasel <albaselofficial@gmail.com>
2023-02-19 13:08:23 -0800

    S

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - The success combination
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i, j;
+for (i = 48; i <= 57; i++)
+{
+for (j = i + 1; j <= 57; j++)
+{
+for (int k = j + 1; k <= 57; ++k)
+{
+if (i != j && j != k)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (i < '7' || j < '8' || k < '9')
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
