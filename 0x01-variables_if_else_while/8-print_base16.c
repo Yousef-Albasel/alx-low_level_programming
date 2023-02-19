dc0289da09f6c9531c6de9f4692627005d28aa40
YousefAlbasel <albaselofficial@gmail.com>
2023-02-19 12:36:41 -0800

    s

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Printing all number from 0 to 9
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+putchar(i);
+for (i = 97; i <= 102; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
