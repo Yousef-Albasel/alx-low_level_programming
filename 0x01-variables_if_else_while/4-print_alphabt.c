23c3c348a2e000b366e707f6195ab39848a2984d
YousefAlbasel <albaselofficial@gmail.com>
2023-02-19 12:21:11 -0800

    S

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - printing all alphabet but for q and e
+ * main - Solution for problem alphABET
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int a;
+int a;
 
-	for (a = 'a'; a <= 'z'; a++)
-		if (a != 'q' && a != 'e')
-		{
-			putchar(a);
-		}
-		putchar('\n');
-	return (0);
+for (a = 'a'; a <= 'z'; a++)
+if (a != 'q' && a != 'e')
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
 }
