79ceb1dcc6244690e962e89905ee1f986cddb9e9
YousefAlbasel <albaselofficial@gmail.com>
2023-02-22 07:31:47 -0800

    S

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,18 +12,26 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n > 98)
+	if (n >= 98)
 	{
 	for (i = n; i >= 98; i--)
 	{
-		printf("%d, ", i);
+		printf("%d", i);
+		if (n == 98)
+			continue;
+		printf(", ");
 	}
+	printf("\n");
 	}
 	else if (n <= 98)
 	{
 	for (i = n; i <= 98; i++)
 	{
-		printf("%d, ", i);
+		printf("%d", i);
+		if (n == 98)
+			continue;
+		printf(", ");
 	}
+	printf("\n");
 	}
 }
