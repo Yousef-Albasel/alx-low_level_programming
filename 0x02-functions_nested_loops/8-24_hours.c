3444ed5b9e9a203da014da8ceabaab881408c36c
YousefAlbasel <albaselofficial@gmail.com>
2023-02-22 07:17:19 -0800

    S

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,10 +8,11 @@
 
 void jack_bauer(void)
 {
-	int n,m;
-	for ( n = 0; n < 24; n++)
+	int n, m;
+
+	for (n = 0; n < 24; n++)
 	{
-		for ( m = 0; m < 60; m++)
+		for (m = 0; m < 60; m++)
 		{
 			_putchar((n / 10) + '0');
 			_putchar((n % 10) + '0');
