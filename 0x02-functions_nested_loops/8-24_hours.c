4a8d04e5d0bd758c62092253e80bd2065217f9fb
YousefAlbasel <albaselofficial@gmail.com>
2023-02-22 07:14:54 -0800

    S

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Check Main
+ * Description: function prints every minute in one day
+ * Return: Nothing
+ */
+
+void jack_bauer(void)
+{
+	int n,m;
+	for ( n = 0; n < 24; n++)
+	{
+		for ( m = 0; m < 60; m++)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
